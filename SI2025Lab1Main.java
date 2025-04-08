ced19e9d9b3c915b4c08d7d5c74da834d65dbc95
Damjan Zimbakov <zimbakovtech@gmail.com>
2025-04-08 18:38:50 +0200

    Implemented task marking as complete by name logic

--- SI2025Lab1Main.java
+++ SI2025Lab1Main.java
@@ -103,7 +103,12 @@ class TaskManager {
 
     // 8. Mark a task as completed by name
     public void markTaskCompleted(String name) {
-        // TODO: Implement completion logic
+        for (Task task : tasks) {
+            if (task.getName().equalsIgnoreCase(name)) {
+                task.complete();
+                return;
+            }
+        }
     }
 
     // 9. Mark all tasks in a category as completed
